fa8004cab9a0371cfef27da02ddde33bc0226a36
27bslash <greenpear68@yahoo.co.uk>
2021-04-19 18:57:56 +0100

    increased idle time on shutdown

--- shutdown.py
+++ shutdown.py
@@ -8,7 +8,7 @@ def shutdown():
     now = datetime.datetime.now().time()
     shutoff_time = datetime.time(4, 15, 0)
     max_shutoff = datetime.time(6, 0, 0)
-    if now > shutoff_time and now < max_shutoff and idle_checker() > 600:
+    if now > shutoff_time and now < max_shutoff and idle_checker() > 1800:
         os.system('shutdown -s')
 
 def idle_checker():
